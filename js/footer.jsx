7eb8e8e997d395cc01a6c678cc2381658ddce000
JCiechanowski93 <j.ciechanowski93@gmail.com>
2018-10-25 10:01:13 +0200

    Footer

--- js/footer.jsx
+++ js/footer.jsx
@@ -5,11 +5,24 @@ class Footer extends Component {
     state = {}
     render() {
         return (<footer className="footer">
-            <div>
-                Jakub Ciechanowski &copy;
+            <div className="myProfile">
+                <div>
+                    Jakub Ciechanowski &copy;
+                </div>
+                <div>
+                    <a href="https://www.linkedin.com/in/jakub-ciechanowski/"><i className="fab fa-linkedin"></i></a>
+                </div>
+                <div>
+                    <a href="https://github.com/JCiechanowski93"><i className="fab fa-github-square"></i></a>
+                </div>
             </div>
-            <div>
-                The ownership of notes and tracks: Cantiamo Tutto Choir
+            <div className="cantiamoTutto">
+                <div>
+                    The ownership of sheet music and tracks: Cantiamo Tutto Choir
+                </div>
+                <div>
+                    <a href="https://www.facebook.com/CantiamoTutto/"><img src="./images/cantiamotutto.jpg" alt="" /></a>
+                </div>
             </div>
         </footer>);
     }
